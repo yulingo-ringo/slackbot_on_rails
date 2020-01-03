00b891190cf418dbc4d758b6a42897406e56db63
starwarrs <yulikamiya@gmail.com>
2020-01-03 23:45:19 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -11,12 +11,44 @@ module  Body
                     builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
                 end
             if @json[:event][:subtype] != "bot_message" #これがないと無限ループになる
+                if json_hash[:event][:text]=="redirect" 
+                    attachments_json = [
+                        {
+                            "fallback": "Upgrade your Slack client to use messages like these.",
+                            "color": "#258ab5",
+                            "attachment_type": "default",
+                            "callback_id": "the_greatest_war",
+                            "actions": [
+                                {
+                                    "name": "choco1",
+                                    "text": "きのこ",
+                                    "value": "kinoko",
+                                    "type": "button"
+                                },
+                                {
+                                    "name": "choco2",
+                                    "text": "たけのこ",
+                                    "value": "takenoko",
+                                    "type": "button"
+                                }
+                            ]
+                        }
+                    ]
+                    body = {
+                        :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
+                        :channel => @json[:event][:channel],#こうするとDM内に返信できます
+                        :text  => "ボタン出てきた？"
+                        :attachments => attachments_json
+                        }
+
+                else 
                 body = {
                         :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
                         :channel => @json[:event][:channel],#こうするとDM内に返信できます
                         :text  => "<https://supership.jp/|🍣>"
                         }
                 conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
+                end
             end
         end
     end
