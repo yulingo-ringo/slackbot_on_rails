d9770bf5ccb528fb2301dfcaefc29b6913d05a1e
starwarrs <yulikamiya@gmail.com>
2020-01-06 12:50:08 +0900

    wip

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -18,7 +18,7 @@ class SlackController < ApplicationController
 
     def action
         body=URI.decode(request.body.read)
-        p body.class
+        p body.to_json.class
         # extend ActiveSupport::JSON.decode(body)
         # how_about = JSON.decode(body)
         # p how_about
