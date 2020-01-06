04eee9ef9f30bf0a6749ddafc98927be40e7d8a9
starwarrs <yulikamiya@gmail.com>
2020-01-06 12:48:18 +0900

    wip

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -18,10 +18,10 @@ class SlackController < ApplicationController
 
     def action
         body=URI.decode(request.body.read)
-        p body["trigger_id"]
-         extend ActiveSupport::JSON.decode(body)
-         how_about = JSON.decode(body)
-         p how_about
+        p body.class
+        # extend ActiveSupport::JSON.decode(body)
+        # how_about = JSON.decode(body)
+        # p how_about
         #p "hello"
         #deleted = request.body.read.delete("'")
         #p deleted
